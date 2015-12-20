13da0cba25de28be3f3f4b8bea8e7ef741720f7b
Javier Cantos <jcantosmoreno@gmail.com>
2015-12-20 12:01:19 +0100

    modificaciones x integración API de openlibrary.org.

--- isbn/isbn/ViewController.swift
+++ isbn/isbn/ViewController.swift
@@ -8,11 +8,15 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var uiTextISBN: UITextField!
+    @IBOutlet weak var uiTextViewJSON: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        self.uiTextISBN.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +24,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    @IBAction func textFieldDoneEditing(sender : UIControl) {
+        self.uiTextISBN.resignFirstResponder()
+        connectTo()
+    }
+    
+    func connectTo() {
+        let library = "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:"
+        let url = NSURL(string:"\(library)\(self.uiTextISBN.text!)")
+        let data : NSData? = NSData(contentsOfURL: url!)
+        let json = NSString(data:data!, encoding:NSUTF8StringEncoding)
+        self.uiTextViewJSON.text = "\(json)"
+    }
 }
 
